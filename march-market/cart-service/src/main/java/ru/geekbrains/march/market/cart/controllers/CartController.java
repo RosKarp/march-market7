af0f4767bb5c7e55fd3564deababe95f41360545
Rostislav <rostkar@gmail.com>
2023-02-25 18:23:06 +0300

    ДЗ 10. Добавлено немного, разбирался со старым.

--- march-market/cart-service/src/main/java/ru/geekbrains/march/market/cart/controllers/CartController.java
+++ march-market/cart-service/src/main/java/ru/geekbrains/march/market/cart/controllers/CartController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.march.market.api.CartDto;
 import ru.geekbrains.march.market.api.StringResponse;
 import ru.geekbrains.march.market.cart.converters.CartConverter;
+import ru.geekbrains.march.market.cart.exceptions.IllegalStateException;
 import ru.geekbrains.march.market.cart.services.CartService;
 import ru.geekbrains.march.market.cart.utils.Cart;
 
@@ -24,27 +25,33 @@ public class CartController {
 
     @GetMapping("/{guestCartId}")
     public CartDto getCurrentCart(@RequestHeader(required = false) String username, @PathVariable String guestCartId) {
-        if(username != null && !cartService.getCurrentCart(guestCartId).getItems().isEmpty()) {
-            Cart authUserCart = cartService.getCurrentCart(username);            // слияние корзин после авторизации
-            Cart anonimCart = cartService.getCurrentCart(guestCartId);
-            authUserCart.getItems().addAll(anonimCart.getItems());
-            authUserCart.setTotalPrice(authUserCart.getTotalPrice().add(anonimCart.getTotalPrice()));
-            anonimCart.clear();
-        }
-        String currentCartId = selectCartId(username, guestCartId);
-        return cartConverter.entityToDto(cartService.getCurrentCart(currentCartId));
+        if (username != null && !username.trim().isEmpty()) {       // вход только авторизованным
+            if (!cartService.getCurrentCart(guestCartId).getItems().isEmpty()) {
+                Cart authUserCart = cartService.getCurrentCart(username);            // слияние корзин после авторизации
+                Cart anonimCart = cartService.getCurrentCart(guestCartId);
+                authUserCart.getItems().addAll(anonimCart.getItems());
+                authUserCart.setTotalPrice(authUserCart.getTotalPrice().add(anonimCart.getTotalPrice()));
+                anonimCart.clear();
+            }
+            String currentCartId = selectCartId(username, guestCartId);
+            return cartConverter.entityToDto(cartService.getCurrentCart(currentCartId));
+        } else { throw new IllegalStateException("Необходима авторизация");}
     }
 
     @GetMapping("/{guestCartId}/add/{productId}")
     public void addProductToCart(@RequestHeader(required = false) String username, @PathVariable String guestCartId, @PathVariable Long productId) {
+        if (username != null && !username.trim().isEmpty()) {       // вход только авторизованным
         String currentCartId = selectCartId(username, guestCartId);
         cartService.addToCart(currentCartId, productId);
+        } else { throw new IllegalStateException("Необходима авторизация");}
     }
 
     @GetMapping("/{guestCartId}/clear")
     public void clearCurrentCart(@RequestHeader(required = false) String username, @PathVariable String guestCartId) {
+        if (username != null && !username.trim().isEmpty()) {       // вход только авторизованным
         String currentCartId = selectCartId(username, guestCartId);
         cartService.clearCart(currentCartId);
+    } else { throw new IllegalStateException("Необходима авторизация");}
     }
 
     private String selectCartId(String username, String guestCartId) {
